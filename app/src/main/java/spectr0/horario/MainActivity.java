4adcda58b54cc53840473cbf6e7d519167347aa5
Spectr0 <miguel_mega24@hotmail.com>
2017-03-31 07:38:51 -0600

    Initial commit

---
+++ app/src/main/java/spectr0/horario/MainActivity.java
@@ -0,0 +1,13 @@
+package spectr0.horario;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
